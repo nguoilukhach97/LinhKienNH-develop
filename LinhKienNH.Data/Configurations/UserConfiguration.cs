19bc382163f5fac742b695eb5c543bdc04bc2197
hungth230297@gmail.com <hungth230297@gmail.com>
2020-06-24 14:59:14 +0700

    create_model-db

--- LinhKienNH.Data/Configurations/UserConfiguration.cs
+++ LinhKienNH.Data/Configurations/UserConfiguration.cs
@@ -15,10 +15,18 @@ namespace LinhKienNH.Data.Configurations
             builder.ToTable("Users");
             builder.HasKey(p=>p.Id);
             builder.Property(x => x.Id).UseIdentityColumn();
-            builder.Property(p => p.UserName).IsRequired();
-            builder.Property(p => p.PassWord).IsRequired();
+            builder.Property(p => p.UserName).IsRequired().HasMaxLength(50);
+            builder.Property(p => p.PassWord).IsRequired().HasMaxLength(32);
             builder.Property(p => p.GroupId).IsRequired();
             builder.HasOne(p => p.UserGroups).WithMany(x => x.Users).HasForeignKey(p => p.GroupId);
+
+            builder.Property(x => x.FullName).HasMaxLength(250);
+            builder.Property(x => x.Email).HasMaxLength(250).IsUnicode(false);
+            builder.Property(x => x.PhoneNumber).HasMaxLength(12).IsUnicode(false);
+            builder.Property(x => x.Address).HasMaxLength(250);
+            builder.Property(x => x.Image).HasMaxLength(250);
+            builder.Property(x => x.Status).HasDefaultValue("1");
+
         }
     }
 }
