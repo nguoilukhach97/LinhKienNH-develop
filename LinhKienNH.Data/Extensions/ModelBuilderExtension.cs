65cbe1d3af4b11ab5379b64edb1fa8e60058dd23
Nguyen Van Hung <hungth230297@gmail.com>
2020-06-24 22:28:31 +0700

    đ

---
+++ LinhKienNH.Data/Extensions/ModelBuilderExtension.cs
@@ -0,0 +1,20 @@
+﻿using LinhKienNH.Data.EF.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace LinhKienNH.Data.Extensions
+{
+    public static class ModelBuilderExtension
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product() { }
+                );
+        }
+    }
+}
