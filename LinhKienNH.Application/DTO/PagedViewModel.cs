7ef38c358a6179ca48e20fd2a3e2ed52ed0bca32
hungth230297@gmail.com <hungth230297@gmail.com>
2020-06-25 18:34:38 +0700

    create structure application

---
+++ LinhKienNH.Application/DTO/PagedViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinhKienNH.Application.DTO
+{
+    public class PagedViewModel
+    {
+
+    }
+}
---
+++ LinhKienNH.Application/ManageCatalog/IManageProductService.cs
@@ -0,0 +1,18 @@
+﻿using LinhKienNH.Application.ManageCatalog.DTOS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinhKienNH.Application.ManageCatalog
+{
+    public interface IManageProductService
+    {
+        int Create(ProductCreateRequest productCreateRequest);
+        int Update(ProductEditRequest productEditRequest);
+        int Delete(int productId);
+
+        List<ProductViewModel> GetAll();
+        List<ProductViewModel> GetAllPaging(string keyWork, int pageIndex, int pageSize);
+        List<produc>
+    }
+}
